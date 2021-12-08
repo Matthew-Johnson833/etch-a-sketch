ebd328ff2202365b9483720fb84bb5effa8575fb
Matthew-Johnson833 <matthew.johnson833@gmail.com>
2021-12-07 18:06:18 -0800

    functional code! more features and styling to come

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function generateDivs() {
         for (let x = 1; x <= 16 /* same variable */; x++) {
             let cell = document.createElement("div"); // every time x increments, create a new cell within the row i
             cell.classList.add("gridSquare");
-            cell.addEventListener('mouseover', () => changeColor()); // create eventListener for each cell
+            cell.addEventListener('mouseover', changeColor); // create eventListener for each cell
             cell.innerText = (i * 16/* same variable */) + x; // remove, this is a counter for how many cells created
             row.appendChild(cell); // add to the row
         }
@@ -22,12 +22,12 @@ function generateDivs() {
 }
 
 // create a function to change the color of an individual cell within a row div, eventListener 'mouseover'
-function changeColor() {
-    // fancy code creation by people way smarter than I, -- https://www.paulirish.com/2009/random-hex-color-code-snippets/
+function changeColor(event) {
+    // fancy code creation by people way knowledgable than I, -- https://www.paulirish.com/2009/random-hex-color-code-snippets/
     let color = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0'); // slightly modified, to ensure a hexcode that is 6 figures long
-    // cell.style.backgroundColor = color;
-    let colorChange = document.getElementsByClassName('cell')
-    colorChange.style.backgroundColor = color; // typeError -- this is null ??
+    let colorChange = document.getElementsByClassName('cell');
+    event.target.style.backgroundColor = color; // event.target to specify exactly where the color needs to be sent. Line below never sent the color to an exact location 
+    //colorChange.style.backgroundColor = color; // typeError -- this is undefined ??
 }
 
 generateDivs();
