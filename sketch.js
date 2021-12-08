d9e59625ed5ec3c8462708d58a01f00dd6fbe727
Matthew-Johnson833 <matthew.johnson833@gmail.com>
2021-12-07 17:16:29 -0800

    grid created, uncaught ErrorType on colorChange

---
+++ sketch.js
@@ -0,0 +1,33 @@
+// This Javascript will create a grid of divs and will allow a user to draw in it with different colors
+
+// initially only create a 16x16 grid, adjustable later.
+function generateDivs() {
+    let container = document.querySelector("#container");
+
+    
+    for (let i = 0; i < 16/* change to variable here */; i++) {
+        let row = document.createElement('div'); // every time i increments, create a new div called row
+        row.classList.add("row");
+        for (let x = 1; x <= 16 /* same variable */; x++) {
+            let cell = document.createElement("div"); // every time x increments, create a new cell within the row i
+            cell.classList.add("gridSquare");
+            cell.addEventListener('mouseover', () => changeColor()); // create eventListener for each cell
+            cell.innerText = (i * 16/* same variable */) + x; // remove, this is a counter for how many cells created
+            row.appendChild(cell); // add to the row
+        }
+        container.appendChild(row); // add the completed row to the container
+    }
+    // this line displayed the actual HTML that was injected by the JS. Not Pretty!
+    // document.getElementById("container").innerText = e.innerHTML;
+}
+
+// create a function to change the color of an individual cell within a row div, eventListener 'mouseover'
+function changeColor() {
+    // fancy code creation by people way smarter than I, -- https://www.paulirish.com/2009/random-hex-color-code-snippets/
+    let color = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0'); // slightly modified, to ensure a hexcode that is 6 figures long
+    // cell.style.backgroundColor = color;
+    let colorChange = document.getElementsByClassName('cell')
+    colorChange.style.backgroundColor = color; // typeError -- this is null ??
+}
+
+generateDivs();
