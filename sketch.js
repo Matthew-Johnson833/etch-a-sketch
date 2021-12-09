cbf570c395fd5220a74d3bcd7fcca6e44009669f
Matthew-Johnson833 <matthew.johnson833@gmail.com>
2021-12-08 17:30:52 -0800

    buttons mostly function, gave controls and the grid their own div.

--- sketch.js
+++ sketch.js
@@ -1,21 +1,38 @@
+// initial grid size 
+//let gridSize = 5;
+
+const gridDiv = document.createElement('div');
+
+let clearBtn = document.querySelector('.clearBtn');
+let changeGridSizeBtn = document.querySelector('.changeGridSizeBtn');
+//buttons.classList.add('buttons');
+
+gridDiv.classList.add('gridDiv');
+container.appendChild(gridDiv);
+let gridSizeVariable = document.getElementsByName("gridSizeSelect")[0].value;
+
+
+// Design Change --> Create all necessary buttons in html, select and modify them as necessary here
+// create the Clear button and make it functional
+
+clearBtn.addEventListener('click', clearGrid); 
+changeGridSizeBtn.addEventListener('click', changeGridSize);
+
 // This Javascript will create a grid of divs and will allow a user to draw in it with different colors
-
-// initially only create a 16x16 grid, adjustable later.
 function generateDivs() {
-    let container = document.querySelector("#container");
-
+    let selectGrid = document.querySelector(".gridDiv");
     
-    for (let i = 0; i < 16/* change to variable here */; i++) {
+    for (let i = 0; i < gridSizeVariable; i++) {
         let row = document.createElement('div'); // every time i increments, create a new div called row
         row.classList.add("row");
-        for (let x = 1; x <= 16 /* same variable */; x++) {
+        for (let x = 1; x <= gridSizeVariable; x++) {
             let cell = document.createElement("div"); // every time x increments, create a new cell within the row i
             cell.classList.add("gridSquare");
             cell.addEventListener('mouseover', changeColor); // create eventListener for each cell
-            cell.innerText = (i * 16/* same variable */) + x; // remove, this is a counter for how many cells created
+            //cell.innerText = (i * gridSize) + x; // remove, this is a counter for how many cells created
             row.appendChild(cell); // add to the row
         }
-        container.appendChild(row); // add the completed row to the container
+        gridDiv.appendChild(row); // add the completed row to the container
     }
     // this line displayed the actual HTML that was injected by the JS. Not Pretty!
     // document.getElementById("container").innerText = e.innerHTML;
@@ -30,4 +47,18 @@ function changeColor(event) {
     //colorChange.style.backgroundColor = color; // typeError -- this is undefined ??
 }
 
+function clearGrid() {
+    gridDiv.innerHTML = "";
+    generateDivs();
+    console.log('this was clicked');
+}
+
+function changeGridSize() { // changes grid size on refresh, not on click
+    gridDiv.innerHTML ="";
+    generateDivs();
+    console.log(gridSizeVariable);
+}
+
+//container.appendChild(buttons);
 generateDivs();
+
